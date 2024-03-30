398255d530f05f2dc6d4962e207c0f123b75a810
Do Nhat <dmnhat.pt@gmail.com>
2024-03-30 11:56:57 +0700

    Update api document.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ import env from "dotenv";
 const app = express();
 env.config();
 
+app.use(express.static("public"));
+
+app.get("/document", (req, res) => {
+  res.render("index.ejs", { port: process.env.PORT });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
