47108f55e8e3461e5f4f0181c697e0523adfe0f6
Do Nhat <dmnhat.pt@gmail.com>
2024-04-11 17:23:24 +0700

    Navigate to Welcome Screen.

--- frontend/lib/features/intro/screens/intro_screen.dart
+++ frontend/lib/features/intro/screens/intro_screen.dart
@@ -1,44 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frontend/constants/global_variables.dart';
+import 'package:frontend/features/intro/screens/welcome_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
+  void _navigateToWelcomeScreen(BuildContext context) {
+    Navigator.of(context).pushNamed(WelcomeScreen.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: GlobalVariables.green,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(
-              'assets/vectors/vector_shuttlecock.svg',
-              width: 100,
-              height: 100,
-            ),
-            const SizedBox(height: 12),
-            RichText(
-              text: TextSpan(
-                text: 'BAD',
-                style: GoogleFonts.alfaSlabOne(
-                  color: GlobalVariables.yellow,
-                  fontSize: 24,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'COURT',
-                    style: GoogleFonts.alfaSlabOne(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  )
-                ],
+    return GestureDetector(
+      onTap: () => _navigateToWelcomeScreen(context),
+      child: Scaffold(
+        backgroundColor: GlobalVariables.green,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(
+                'assets/vectors/vector_shuttlecock.svg',
+                width: 100,
+                height: 100,
               ),
-            ),
-          ],
+              const SizedBox(height: 12),
+              RichText(
+                text: TextSpan(
+                  text: 'BAD',
+                  style: GoogleFonts.alfaSlabOne(
+                    color: GlobalVariables.yellow,
+                    fontSize: 24,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'COURT',
+                      style: GoogleFonts.alfaSlabOne(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- frontend/lib/router.dart
+++ frontend/lib/router.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/features/intro/screens/welcome_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
+    case WelcomeScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const WelcomeScreen(),
+      );
     default:
       return MaterialPageRoute(
         settings: routeSettings,
