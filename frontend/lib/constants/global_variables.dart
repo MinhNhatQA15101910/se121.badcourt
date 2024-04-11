653f50e4fbc6a7014049f5a91cff4636584d6290
Do Nhat <dmnhat.pt@gmail.com>
2024-04-11 17:32:02 +0700

    Create color scheme for the app.

--- frontend/lib/constants/global_variables.dart
+++ frontend/lib/constants/global_variables.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 class GlobalVariables {
   // Colors
-  static const Color green = Color(0xFF23C16B);
+  static const Color black = Color(0xFF000000);
+  static const Color blackGrey = Color(0xFF27272A);
+  static const Color darkGrey = Color(0xFF808089);
+  static const Color grey = Color(0xFFEBEBF0);
+  static const Color lightGrey = Color(0xFFF2F4F5);
+  static const Color defaultColor = Color(0xFFFAFAFA);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkRed = Color(0xFFBF1D28);
+  static const Color lightRed = Color(0xFFFFDBDE);
+  static const Color red = Color(0xFFFF424F);
+  static const Color darkBlue = Color(0xFF0D5BB5);
+  static const Color lightBlue = Color(0xFFDBEEFF);
+  static const Color darkYellow = Color(0xFFCC8100);
   static const Color yellow = Color(0xFFFFBA49);
+  static const Color lightYellow = Color(0xFFFFF5C7);
+  static const Color darkGreen = Color(0xFF007D3A);
+  static const Color lightGreen = Color(0xFFD7FAE0);
+  static const Color green = Color(0xFF23C16B);
 }
