b689c26f32fcf1a5a2cb35169c6351e05a057511
Do Nhat <dmnhat.pt@gmail.com>
2024-04-13 16:38:26 +0700

    Create log in route.

--- backend/routers/auth_router.js
+++ backend/routers/auth_router.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
-import env from "dotenv";
 import express from "express";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
@@ -33,6 +33,30 @@ authRouter.post("/user/signup", async (req, res) => {
     });
     user = await user.save();
     res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Log in route
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exist!" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.PASSWORD_KEY);
+    res.json({ token, ...user._doc });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
