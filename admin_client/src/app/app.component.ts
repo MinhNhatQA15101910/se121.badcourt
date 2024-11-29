b7617ad0cff123c8849bb2b5a99f1150891902a5
Do Nhat <dmnhat.pt@gmail.com>
2024-11-29 12:22:14 +0700

    Re-initialize admin client.

--- admin_client/src/app/app.component.ts
+++ admin_client/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BadCourt';
+  title = 'admin_client';
 }
