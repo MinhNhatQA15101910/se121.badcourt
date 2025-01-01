75de03c072a25331035e632b5e82ffe79dfbca3d
duyvi <nhatduy13nhatduy@gmail.com>
2025-01-02 02:02:16 +0700

    Update message

--- frontend/lib/features/message/pages/message_detail_screen.dart
+++ frontend/lib/features/message/pages/message_detail_screen.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:frontend/common/services/socket_service.dart';
+import 'package:frontend/constants/global_variables.dart';
 import 'package:frontend/features/message/services/message_service.dart';
 import 'package:frontend/features/message/widgets/message_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/providers/user_provider.dart';
+import 'package:intl/intl.dart';
 
 class MessageDetailScreen extends StatefulWidget {
   static const String routeName = '/messageDetailScreen';
@@ -23,12 +25,11 @@ class MessageDetailScreen extends StatefulWidget {
 
 class _MessageDetailScreenState extends State<MessageDetailScreen> {
   final TextEditingController _messageController = TextEditingController();
-  final ImagePicker _picker = ImagePicker();
-  final List<File> _imageFiles = [];
   final List<Map<String, dynamic>> _messages = [];
-
   final _messageService = MessageService();
   final _socketService = SocketService();
+  final ImagePicker _picker = ImagePicker();
+  List<File>? _imageFiles = [];
 
   bool _isLoading = true;
   String roomId = "";
@@ -44,6 +45,18 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
     _messageController.dispose();
     _socketService.socket?.dispose();
     super.dispose();
+  }
+
+  Future<void> _pickImages() async {
+    final List<XFile>? selectedImages = await _picker.pickMultiImage();
+    if (selectedImages != null) {
+      setState(() {
+        // Convert XFile to File and add images
+        _imageFiles?.addAll(
+          selectedImages.map((xfile) => File(xfile.path)).toList(),
+        );
+      });
+    }
   }
 
   Future<void> _initializeServices() async {
@@ -65,13 +78,10 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
       // Listen for new messages
       _socketService.onNewMessage((data) {
         setState(() {
-          final timestamp = data['createdAt'];
-          final timeFormatted = DateFormat('HH:mm')
-              .format(DateTime.fromMillisecondsSinceEpoch(timestamp));
           _messages.insert(0, {
-            'isSender': false,
-            'message': data['content'],
-            'time': timeFormatted,
+            'isSender': data['senderId'] == userProvider.user.id,
+            'message': data['content'] ?? '',
+            'time': data['createdAt'], // Store as int
           });
         });
       });
@@ -97,14 +107,10 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
       setState(() {
         _messages.clear();
         _messages.addAll(messages.map<Map<String, dynamic>>((msg) {
-          final timestamp = msg['createdAt'];
-          final timeFormatted = DateFormat('HH:mm')
-              .format(DateTime.fromMillisecondsSinceEpoch(timestamp));
-
           return {
-            'isSender': msg['isSender'],
-            'message': msg['content'],
-            'time': timeFormatted,
+            'isSender': msg['senderId'] == userProvider.user.id,
+            'message': msg['content'] ?? '',
+            'time': msg['createdAt'] as int? ?? 0,
           };
         }).toList());
 
@@ -126,12 +132,8 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
     }
 
     final content = _messageController.text.trim();
-
-    setState(() {
-      _fetchMessages(roomId);
-    });
-
     _messageController.clear();
+    _imageFiles = [];
 
     try {
       // REST API for saving the message
@@ -167,33 +169,171 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
                     itemCount: _messages.length,
                     itemBuilder: (context, index) {
                       final message = _messages[index];
-                      return MessageWidget(
-                        isSender: message['isSender'] ?? false,
-                        message: message['message'] ?? '',
-                        time: message['time'] ?? '',
+                      final nextMessage =
+                          index > 0 ? _messages[index - 1] : null;
+
+                      return Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: MessageWidget(
+                          isSender: message['isSender'] ?? false,
+                          message: message['message'] ?? '',
+                          time: message['time'],
+                          nextTime: nextMessage?['time'],
+                        ),
                       );
                     },
                   ),
                 ),
-                const Divider(height: 1),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _messageController,
-                          decoration: const InputDecoration(
-                            hintText: 'Type a message...',
-                            border: OutlineInputBorder(),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        width: 1,
+                        color: GlobalVariables.grey,
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    child: Column(
+                      children: [
+                        if (_imageFiles != null && _imageFiles!.isNotEmpty)
+                          Container(
+                            margin: EdgeInsets.only(bottom: 8),
+                            width: double
+                                .infinity, // Ensure it takes up the full width
+                            child: GridView.builder(
+                              shrinkWrap:
+                                  true, // Ensure the grid only takes up as much space as needed
+                              physics:
+                                  NeverScrollableScrollPhysics(), // Prevent scrolling within the grid
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 5,
+                                crossAxisSpacing:
+                                    4, // Horizontal spacing between images
+                                mainAxisSpacing:
+                                    4, // Vertical spacing between images
+                              ),
+                              itemCount: _imageFiles!.length,
+                              itemBuilder: (context, index) {
+                                final imageFile = _imageFiles![index];
+                                return Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(6),
+                                      child: Image.file(
+                                        imageFile,
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      top: 4,
+                                      right: 4,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            _imageFiles?.removeAt(index);
+                                          });
+                                        },
+                                        child: Container(
+                                          padding: const EdgeInsets.all(4),
+                                          decoration: BoxDecoration(
+                                            color: Colors.black.withOpacity(
+                                                0.5), // Semi-transparent background for the "X"
+                                            borderRadius: BorderRadius.circular(
+                                                12), // Rounded corners for the "X"
+                                          ),
+                                          child: const Icon(
+                                            Icons.close,
+                                            size: 16,
+                                            color: Colors
+                                                .white, // White icon color
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
                           ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8),
+                              child: GestureDetector(
+                                onTap: _pickImages,
+                                child: const Icon(
+                                  Icons.add_photo_alternate_outlined,
+                                  color: GlobalVariables.green,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            SizedBox(width: 8),
+                            Expanded(
+                              child: TextFormField(
+                                controller: _messageController,
+                                maxLines: 4,
+                                minLines: 1,
+                                decoration: InputDecoration(
+                                  hintText: 'Type your message...',
+                                  hintStyle: GoogleFonts.inter(
+                                    color: GlobalVariables.darkGrey,
+                                    fontSize: 16,
+                                  ),
+                                  contentPadding: const EdgeInsets.all(16),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: GlobalVariables.lightGreen,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    borderSide: BorderSide(
+                                      color: GlobalVariables.lightGreen,
+                                    ),
+                                  ),
+                                ),
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8),
+                              child: GestureDetector(
+                                onTap: () {
+                                  if (_messageController.text
+                                          .trim()
+                                          .isNotEmpty ||
+                                      _imageFiles!.isNotEmpty) {
+                                    _sendMessage();
+                                  }
+                                },
+                                child: const Icon(
+                                  Icons.send,
+                                  color: GlobalVariables.green,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.send),
-                        onPressed: _sendMessage,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
--- frontend/lib/features/message/widgets/message_widget.dart
+++ frontend/lib/features/message/widgets/message_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class MessageWidget extends StatelessWidget {
   final bool isSender;
   final String message;
-  final String time;
+  final int time; // Timestamp in milliseconds
+  final int? nextTime; // Timestamp of the next message
   final String? imageUrl;
 
   const MessageWidget({
@@ -11,51 +14,128 @@ class MessageWidget extends StatelessWidget {
     required this.isSender,
     required this.message,
     required this.time,
+    this.nextTime,
     this.imageUrl,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
-      child: Column(
-        crossAxisAlignment:
-            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: [
-          if (imageUrl != null)
-            Container(
-              margin: EdgeInsets.only(bottom: 8),
-              width: 200,
-              height: 120,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                image: DecorationImage(
-                  image: NetworkImage(imageUrl!),
-                  fit: BoxFit.cover,
+    final formattedTime =
+        DateFormat('HH:mm').format(DateTime.fromMillisecondsSinceEpoch(time));
+
+    final currentDate = DateTime.fromMillisecondsSinceEpoch(time);
+    final nextDate = nextTime != null
+        ? DateTime.fromMillisecondsSinceEpoch(nextTime!)
+        : null;
+
+    final isSameMinute = nextDate != null &&
+        currentDate.year == nextDate.year &&
+        currentDate.month == nextDate.month &&
+        currentDate.day == nextDate.day &&
+        currentDate.hour == nextDate.hour &&
+        currentDate.minute == nextDate.minute;
+
+    final isDifferentDay = nextDate != null &&
+        (currentDate.year != nextDate.year ||
+            currentDate.month != nextDate.month ||
+            currentDate.day != nextDate.day);
+
+    return Column(
+      crossAxisAlignment:
+          isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+      children: [
+        Align(
+          alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment:
+                isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              if (imageUrl != null)
+                Container(
+                  margin: const EdgeInsets.only(bottom: 8),
+                  width: 200,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                      image: NetworkImage(imageUrl!),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
+              Container(
+                margin: EdgeInsets.only(top: 4),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: isSender ? Colors.green[100] : Colors.grey[300],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(isSender ? 12 : 0),
+                    topRight: Radius.circular(isSender ? 0 : 12),
+                    bottomLeft: const Radius.circular(12),
+                    bottomRight: const Radius.circular(12),
+                  ),
+                ),
+                child: Text(message),
               ),
-            ),
-          Container(
-            margin: EdgeInsets.only(bottom: 8),
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: isSender ? Colors.green[100] : Colors.grey[300],
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(isSender ? 12 : 0),
-                topRight: Radius.circular(isSender ? 0 : 12),
-                bottomLeft: Radius.circular(12),
-                bottomRight: Radius.circular(12),
-              ),
-            ),
-            child: Text(message),
+              if (!isSameMinute)
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 8,
+                  ),
+                  child: Text(
+                    formattedTime,
+                    style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ),
+            ],
           ),
-          Text(
-            time,
-            style: TextStyle(fontSize: 12, color: Colors.grey),
+        ),
+        if (nextTime == null) const SizedBox(height: 12),
+        if (isDifferentDay)
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              children: [
+                // Left divider
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 64,
+                    ),
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                      endIndent: 10,
+                    ),
+                  ),
+                ),
+                // Centered date text
+                Text(
+                  DateFormat('MMM, dd, yyyy')
+                      .format(currentDate), // e.g., Aug, 12, 2024
+                  style: GoogleFonts.inter(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
+                ),
+                // Right divider
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      right: 64,
+                    ),
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                      indent: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 10),
-        ],
-      ),
+      ],
     );
   }
 }
