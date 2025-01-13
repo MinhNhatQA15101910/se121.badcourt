71f5b969b4ee468fbfa825500d6c853329ec28a8
Do Nhat <dmnhat.pt@gmail.com>
2025-01-13 17:13:54 +0700

    Done seed facility data.

--- API/src/data/data.ts
+++ API/src/data/data.ts
@@ -54,3 +54,121 @@ export const userData = [
     password: "",
   },
 ];
+
+export const facilityData = [
+  {
+    name: "Sân Cầu Lông TamBo",
+    facebookUrl:
+      "https://www.facebook.com/pages/S%C3%A2n%20C%C3%A2%CC%80u%20L%C3%B4ng%20Tambo/287484124787156/",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress:
+      "Số 15A/1 Hẻm 51, Đường 3-2, Phường An Khánh, Ninh Kiều, Cần Thơ, Vietnam",
+    province: "Thành phố Cần Thơ",
+    location: {
+      type: "Point",
+      coordinates: [105.7538001, 10.0276055],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0702853345",
+      citizenId: "082203001442",
+    },
+  },
+  {
+    name: "Sân Cầu Lông ĐHCT",
+    facebookUrl: "https://www.facebook.com/groups/1476641239790497/",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress:
+      "2QJ9+V8R, Unnamed Rd, Xuân Khánh, Ninh Kiều, Cần Thơ, Vietnam",
+    province: "Thành phố Cần Thơ",
+    location: {
+      type: "Point",
+      coordinates: [105.7656973, 10.0322489],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0388815552",
+      citizenId: "082203001442",
+    },
+  },
+  {
+    name: "Sân Cầu Lông Trần Não",
+    facebookUrl: "https://www.facebook.com/share/qkijJ3VG8knwf6qs/",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress:
+      "22 Đ. Số 24, Bình An, Quận 2, Thành phố Hồ Chí Minh, Vietnam",
+    province: "Thành phố Hồ Chí Minh",
+    location: {
+      type: "Point",
+      coordinates: [106.4250779, 10.7944488],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0962427872",
+      citizenId: "082203001442",
+    },
+  },
+  {
+    name: "Sân cầu lông THPT MARIE CURIE",
+    facebookUrl:
+      "https://www.facebook.com/pages/Clb-C%E1%BA%A7u-L%C3%B4ng-Tr%C6%B0%E1%BB%9Dng-Thpt-Marie-Curie/1104998279561562",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress:
+      "26 Lê Quý Đôn, Võ Thị Sáu, Quận 3, Thành phố Hồ Chí Minh, Vietnam",
+    province: "Thành phố Hồ Chí Minh",
+    location: {
+      type: "Point",
+      coordinates: [106.6876871, 10.7820299],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0979464649",
+      citizenId: "082203001442",
+    },
+  },
+  {
+    name: "Trung Tâm TDTT Sân Cầu Lông",
+    facebookUrl: "https://vi-vn.facebook.com/caulong.minhkhai/",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress: "521 P. Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội, Vietnam",
+    province: "Thành phố Hà Nội",
+    location: {
+      type: "Point",
+      coordinates: [105.8702438, 21.0007026],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0936129898",
+      citizenId: "082203001442",
+    },
+  },
+  {
+    name: "Sân Cầu Lông Tiến Dinh Sport",
+    facebookUrl: "https://www.facebook.com/sancaulongtiendinhsport/",
+    description: "Test description",
+    policy: "Test policy",
+    detailAddress:
+      "Tòa B, 488 P. Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội 100000, Vietnam",
+    province: "Thành phố Hà Nội",
+    location: {
+      type: "Point",
+      coordinates: [105.8659253, 20.998275],
+    },
+    managerInfo: {
+      fullName: "Đỗ Nhật",
+      email: "dmnhat.pt@gmail.com",
+      phoneNumber: "0869987669",
+      citizenId: "082203001442",
+    },
+  },
+];
--- API/src/data/seedData.ts
+++ API/src/data/seedData.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/user";
-import { userData } from "./data";
+import { facilityData, userData } from "./data";
 import { hashSync } from "bcrypt";
 import {
   CLOUDINARY_API_KEY,
@@ -9,6 +9,7 @@ import {
   SALT_ROUNDS,
 } from "../secrets";
 import { v2 as cloudinary } from "cloudinary";
+import Facility from "../models/facility";
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
@@ -19,6 +20,7 @@ cloudinary.config({
 export const seedData = async () => {
   try {
     await seedUsers();
+    await seedFacilities();
   } catch (error) {
     console.error("Error seeding data:", error);
     mongoose.connection.close();
@@ -51,5 +53,122 @@ const seedUsers = async () => {
       await newUser.save();
     }
     console.log("Users seeded successfully");
+  }
+};
+
+const seedFacilities = async () => {
+  const facilities = await Facility.find();
+  if (facilities.length === 0) {
+    const users = await User.find();
+    let index = 1;
+    for (let facility of facilityData) {
+      let newFacility = new Facility(facility);
+      newFacility.userId = users[index - 1]._id.toString();
+      newFacility = await newFacility.save();
+
+      let managerInfo = newFacility.managerInfo;
+
+      // Upload facility images
+      for (let i = 1; i <= 5; i++) {
+        let result = await cloudinary.uploader.upload(
+          `src/data/resources/facilities/${index}/${i}.jpg`,
+          {
+            folder: `BadCourt-prod/facilities/${newFacility._id}/facility_images`,
+            resource_type: "image",
+          }
+        );
+
+        newFacility.facilityImages.push({
+          url: result.url,
+          publicId: result.public_id,
+          isMain: i === 1,
+          type: "image",
+        });
+      }
+
+      // Upload citizen image front
+      let result = await cloudinary.uploader.upload(
+        `src/data/resources/facilities/citizen_images/1.jpg`,
+        {
+          folder: `BadCourt-prod/facilities/${newFacility._id}/citizen_images`,
+          resource_type: "image",
+        }
+      );
+      managerInfo!.citizenImageFront = {
+        url: result.url,
+        publicId: result.public_id,
+        isMain: true,
+        type: "image",
+      };
+
+      // Upload citizen image back
+      result = await cloudinary.uploader.upload(
+        `src/data/resources/facilities/citizen_images/2.jpg`,
+        {
+          folder: `BadCourt-prod/facilities/${newFacility._id}/citizen_images`,
+          resource_type: "image",
+        }
+      );
+      managerInfo!.citizenImageBack = {
+        url: result.url,
+        publicId: result.public_id,
+        isMain: true,
+        type: "image",
+      };
+
+      // Upload bank card image front
+      result = await cloudinary.uploader.upload(
+        `src/data/resources/facilities/bank_card_images/1.jpg`,
+        {
+          folder: `BadCourt-prod/facilities/${newFacility._id}/bank_card_images`,
+          resource_type: "image",
+        }
+      );
+      managerInfo!.bankCardFront = {
+        url: result.url,
+        publicId: result.public_id,
+        isMain: true,
+        type: "image",
+      };
+
+      // Upload bank card image back
+      result = await cloudinary.uploader.upload(
+        `src/data/resources/facilities/bank_card_images/2.jpg`,
+        {
+          folder: `BadCourt-prod/facilities/${newFacility._id}/bank_card_images`,
+          resource_type: "image",
+        }
+      );
+      managerInfo!.bankCardBack = {
+        url: result.url,
+        publicId: result.public_id,
+        isMain: true,
+        type: "image",
+      };
+
+      // Upload business license images
+      for (let i = 1; i <= 10; i++) {
+        let result = await cloudinary.uploader.upload(
+          `src/data/resources/facilities/business_license_images/${i}.jpg`,
+          {
+            folder: `BadCourt-prod/facilities/${newFacility._id}/business_license_images`,
+            resource_type: "image",
+          }
+        );
+
+        managerInfo!.businessLicenseImages.push({
+          url: result.url,
+          publicId: result.public_id,
+          isMain: i === 1,
+          type: "image",
+        });
+      }
+
+      newFacility.managerInfo = managerInfo;
+      await newFacility.save();
+
+      index++;
+    }
+    console.log("Facilities seeded successfully");
   }
 };
