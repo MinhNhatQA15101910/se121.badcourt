eea7fb7fb88cd25e1f11625bbef7c3de3e736a6e
Do Nhat <dmnhat.pt@gmail.com>
2025-03-06 16:12:31 +0700

    Add SeedData class.

--- BadCourtAPI/BadCourtAPI/Entities/User.cs
+++ BadCourtAPI/BadCourtAPI/Entities/User.cs
@@ -4,6 +4,7 @@ namespace BadCourtAPI.Entities;
 
 public class User : IdentityUser<Guid>
 {
+    public required string FullName { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public ICollection<UserRole> UserRoles { get; set; } = [];
--- BadCourtAPI/BadCourtAPI/Program.cs
+++ BadCourtAPI/BadCourtAPI/Program.cs
@@ -1,4 +1,7 @@
+using BadCourtAPI.Data;
+using BadCourtAPI.Entities;
 using BadCourtAPI.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,5 +10,20 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<Role>>();
+
+    await Seed.SeedUsersAsync(userManager, roleManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
