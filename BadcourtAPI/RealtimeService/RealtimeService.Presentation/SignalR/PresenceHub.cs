9a2549f868f048826c297c1b97edfe2b16850247
Do Nhat <dmnhat.pt@gmail.com>
2025-05-19 22:54:24 +0700

    Adding presence tracker.

--- BadcourtAPI/RealtimeService/RealtimeService.Presentation/SignalR/PresenceHub.cs
+++ BadcourtAPI/RealtimeService/RealtimeService.Presentation/SignalR/PresenceHub.cs
@@ -5,16 +5,34 @@ using RealtimeService.Presentation.Extensions;
 namespace RealtimeService.Presentation.SignalR;
 
 [Authorize]
-public class PresenceHub : Hub
+public class PresenceHub(PresenceTracker presenceTracker) : Hub
 {
     public override async Task OnConnectedAsync()
     {
-        await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserId());
+        if (Context.User is null)
+        {
+            throw new HubException("Cannot get current user claims");
+        }
+
+        await presenceTracker.UserConnected(Context.User.GetUserId().ToString(), Context.ConnectionId);
+        await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUserId());
+
+        var onlineUsers = await presenceTracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", onlineUsers);
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
+        if (Context.User is null)
+        {
+            throw new HubException("Cannot get current user claims");
+        }
+
+        await presenceTracker.UserDisconnected(Context.User.GetUserId().ToString(), Context.ConnectionId);
         await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUserId());
+
+        var onlineUsers = await presenceTracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", onlineUsers);
 
         await base.OnDisconnectedAsync(exception);
     }
