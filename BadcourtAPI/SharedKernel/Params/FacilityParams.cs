aca679aac49e97ae23a571e95c4c7c91b0aee2cb
Do Nhat <dmnhat.pt@gmail.com>
2025-06-23 21:56:27 +0700

    Adding facility search.

--- BadcourtAPI/SharedKernel/Params/FacilityParams.cs
+++ BadcourtAPI/SharedKernel/Params/FacilityParams.cs
@@ -9,6 +9,7 @@ public class FacilityParams : PaginationParams
     public string? Province { get; set; }
     public decimal MinPrice { get; set; } = decimal.MinValue;
     public decimal MaxPrice { get; set; } = decimal.MaxValue;
+    public string? Search { get; set; }
     public string OrderBy { get; set; } = "registeredAt";
     public string SortBy { get; set; } = "desc";
 }
