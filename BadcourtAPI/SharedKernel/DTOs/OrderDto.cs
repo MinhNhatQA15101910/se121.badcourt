70979feac8994dd5738bebaea3e2a6f10c2ad9be
Do Nhat <dmnhat.pt@gmail.com>
2025-06-23 23:25:00 +0700

    Adding userId to OrderDto and updating the filter logic.

--- BadcourtAPI/SharedKernel/DTOs/OrderDto.cs
+++ BadcourtAPI/SharedKernel/DTOs/OrderDto.cs
@@ -3,6 +3,7 @@ namespace SharedKernel.DTOs;
 public class OrderDto
 {
     public Guid Id { get; set; }
+    public Guid UserId { get; set; }
     public string FacilityName { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public decimal Price { get; set; }
