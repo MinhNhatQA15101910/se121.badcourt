72172d875e951632c5851e8fb9ce514040bcf1b4
Do Nhat <dmnhat.pt@gmail.com>
2025-07-10 13:57:59 +0700

    Updating CORS.

--- BadcourtAPI/AdminService/AdminService.Presentation/Program.cs
+++ BadcourtAPI/AdminService/AdminService.Presentation/Program.cs
@@ -6,7 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration)
     .AddIdentityServices(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost1311", policy =>
+    {
+        policy.WithOrigins("http://localhost:1311")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowLocalhost1311");
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
